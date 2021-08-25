7a71313fab910a5e6867b5fe0d72a5974c075e8c
eduardoribeiro <eribeiro@icpaitalnetwork.com>
2021-08-25 15:00:10 +0100

    Change test component layout

--- src/Components/Shared/TestComponent/TestComponent.tsx
+++ src/Components/Shared/TestComponent/TestComponent.tsx
@@ -9,10 +9,12 @@ const TestComponent: React.FC<TestComponentProps> = ({ theme }) => (
     data-testid="test-component"
     className={`test-component test-component-${theme}`}
   >
-    <h1 className="heading">I'm the test component</h1>
-    <h2>ICapital Network</h2>
-    <h1 className="heading">Version-3</h1>
-    <h2>ICapital Network-V3</h2>
+    <h1 className="heading">iCapital Network</h1>
+    <h2>I'm the test component</h2>
+    <div className="footer">
+      <h6>Version 2.0.3</h6>
+      <h6>iCapital Network 2021</h6>
+    </div>
   </div>
 );
 
