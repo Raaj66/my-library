488e710a38729491e15e7ab838fa0710a0057e49
eduardoribeiro <eribeiro@icpaitalnetwork.com>
2021-08-25 15:05:28 +0100

    Update version

--- src/Components/Shared/TestComponent/TestComponent.tsx
+++ src/Components/Shared/TestComponent/TestComponent.tsx
@@ -10,9 +10,9 @@ const TestComponent: React.FC<TestComponentProps> = ({ theme }) => (
     className={`test-component test-component-${theme}`}
   >
     <h1 className="heading">iCapital Network</h1>
-    <h2>I'm the test component</h2>
+    <h2>I'm the test component in {process.env.REACT_APP_NAME} </h2>
     <div className="footer">
-      <h6>Version 2.0.3</h6>
+      <h6>Version {process.env.REACT_APP_VERSION}</h6>
       <h6>iCapital Network 2021</h6>
     </div>
   </div>
