acf89651f09f10c345393a42039a87ae2f487224
Ayaskanta Samal <silu.samal@gmail.com>
2021-09-24 10:26:56 -0400

    [RCL-78] external dependencies changes (#45)
    Co-authored-by: Ayaskanta Samal <ayaskantasamal@Ayaskantas-MacBook-Pro.local>

--- rollup.config.js
+++ rollup.config.js
@@ -125,4 +125,9 @@ export default {
       ],
     }),
   ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.devDependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
 };
